179d87d942efca3d624c1c5269ce867944de76db
yxd <yxd@XD-PC>
2017-02-09 10:32:04 +0800

    补充wx.login方法的参数

--- typings/weapp.d.ts
+++ typings/weapp.d.ts
@@ -1976,6 +1976,14 @@ declare namespace wx {
          * 接口调用成功的回调函数
          */
         success?: (res?: LoginResult) => void;
+        /**
+         * 接口调用失败的回调函数
+         */
+        fail?: (res?: LoginResult) => void;
+        /**
+         * 接口调用结束的回调函数（调用成功、失败都会执行）
+         */
+        complete?: () => void;
     }
 
     /**
